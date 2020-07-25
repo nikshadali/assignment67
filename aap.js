d06b7aa0305976cd63f6b2db1576d571e333434a
nikshadali <nikali92100>
2020-07-25 08:42:15 -0700

    upload file

---
+++ aap.js
@@ -0,0 +1,109 @@
+//***********************chapter-38 t0 44**************Task 1 **********************************************/
+
+// function foo(a,b){
+//     result = Math.pow(a,b);
+//      console.log(result)
+//     }
+// foo(4,2);
+
+//***********************chapter-38 t0 44**************Task 2 **********************************************/
+
+// function leapYear(year){
+// var year = year;
+// if(year % 100 != 0 && year % 4 === 0 || year % 400 === 0){
+//     console.log(true);
+// }
+// else{
+
+// console.log(false);
+
+// }
+// }
+// leapYear(2012)
+// leapYear(2016)
+// leapYear(2020)
+
+//***********************chapter-38 t0 44**************Task 4 *******************************************/
+
+// var math = +prompt("Enter Math Subject No");
+// var eng = +prompt("Enter Eng Subject No");
+// var urdu = +prompt("Enter Urdu Subject No");
+
+// function ave(math,eng,urdu){
+//     var average = (math + eng + urdu)/300*100
+//      console.log(average.toFixed(2))
+// }
+
+// ave(math,eng,urdu)
+
+//***********************chapter-38 t0 44**************Task 5 *******************************************/
+
+// var a = "Pakistan is our country we love pakistan";
+// var b = a.indexOf("o");
+// console.log(b)
+
+
+//***********************chapter-38 t0 44**************Task 6 *******************************************/
+
+// var a = ["We love our countary pakistan" ]
+// var b =a.replace(/"aeiou"/g,'');
+// console.log(b);
+
+//***********************chapter-38 t0 44**************Task 7 *******************************************/
+
+
+
+//***********************chapter-38 t0 44**************Task 8 *******************************************/
+// var a = +prompt("Enter distance between two cities in km")
+
+// function meter(a){
+//    var met = a * 1000
+//     console.log("in Meters "+met)
+// }
+
+// function feet(a){
+//     var met = a * 3280.84
+//      console.log("in feet "+met)
+//  }
+//  function inch(a){
+//     var met = a * 39370.1
+//      console.log("in Inch "+met)
+//  }
+//  function centi(a){
+//     var met = a * 100000
+//      console.log("in Centimeter "+met)
+//  }
+// meter(a)
+// feet(a)
+// inch(a)
+// centi(a)
+
+//***********************chapter-38 t0 44**************Task 9 *******************************************/
+
+
+//***********************chapter-38 t0 44**************Task 10 *******************************************/
+
+
+// var amount= +prompt("Enter Amount in figure");
+// var handerad = amount /100
+// handerad = handerad.toFixed(0)
+// var fifty =  (amount % 100)/50;
+// fifty = fifty.toFixed(0)
+// var ten =  (((amount % 100) % 50) / 10);
+// ten = ten.toFixed(0)
+
+
+// console.log("You have " + handerad + " Handerd notes " + fifty +" fifty notes " + ten +" Ten notes ")
+
+//***********************chapter-43 t0 48**************Task 1 *******************************************/
+// function addAlert(){
+//     alert("you have clicked link")
+// }
+
+//***********************chapter-43 t0 48**************Task 2 *******************************************/
+
+// function message(){
+//     alert("Thank you for Purchasing Phone from us");
+// }
+
+//***********************chapter-43 t0 48**************Task 3 *******************************************/
